29430a294228678bc6b5c98e0785f8401997bdf0
Hung Le Huy <63230297+Trumpiter-max@users.noreply.github.com>
2023-04-13 21:10:58 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Secure Web based with AES
+
+## Table of contents
+
+## Get started
+
+### Environment
+
+- Python version: 3.10.9
+- OS environment: Windows 11 version 22H2
--- aes_mode_gcm.py
+++ aes_mode_gcm.py
@@ -7,119 +7,53 @@ from cryptography.hazmat.primitives.ciphers.algorithms import AES
 from cryptography.hazmat.primitives.ciphers.modes import GCM
 from cryptography.hazmat.primitives.hashes import SHA256
 
-def aes_gcm_authenticated_decryption(cipherkey, iv, auth_tag, associated_data, ciphertext, private_key):
-    # Decrypt AES key with RSA private key
-    oaep_padding = padding.OAEP(mgf=padding.MGF1(algorithm=SHA256()), algorithm=SHA256(), label=None)
-    key = private_key.decrypt(cipherkey, oaep_padding)
-    aes_gcm_decryptor = Cipher(AES(key), GCM(iv, auth_tag)).decryptor()
-    aes_gcm_decryptor.authenticate_additional_data(associated_data)
-    recovered_plaintext = aes_gcm_decryptor.update(ciphertext) + aes_gcm_decryptor.finalize()
-    return recovered_plaintext
-
-def signature_verification(message, signature, pss_padding, sha256, public_key):
-    try:
-        public_key.verify(signature, message, pss_padding, sha256)
-    except cryptography.exceptions.InvalidSignature:
-        # Should not happen
-        assert False
-    else:
-        assert True
-
-if __name__ == "__main__":
-
-    # RSA-PSS
-    sha256 = hashes.SHA256()
-    pss_padding = padding.PSS(mgf=padding.MGF1(sha256), salt_length=padding.PSS.MAX_LENGTH)
-
-    # Recipient's private key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-
-    # Public key to make available to sender
-    public_key = private_key.public_key()
-
-    # 256-bit symmetric key
-    key = os.urandom(256 // 8)
-
-    # For AES GCM, NIST recommends 96 bit IVs
-    iv = os.urandom(96 // 8)
-
-    # Our message to be kept confidential
-    plaintext = b'Fundamental Cryptography in Python'
-
-    # Associated data
-    associated_data = b'Context of using AES GCM'
-
-    # Signature creation
-    signature = private_key.sign(plaintext, pss_padding, sha256)
-
-    # Signature verification
-    signature_verification(plaintext, signature, pss_padding, sha256, public_key)
-
+def aes_gcm_authenticated_encryption(key, iv, associated_data, plaintext):
     # Encrypt the plaintext (no padding required for GCM)
     aes_gcm_encryptor = Cipher(AES(key), GCM(iv)).encryptor()
     aes_gcm_encryptor.authenticate_additional_data(associated_data)
     ciphertext = aes_gcm_encryptor.update(plaintext) + aes_gcm_encryptor.finalize()
+    global auth_tag
     auth_tag = aes_gcm_encryptor.tag
-    # Encrypt AES key with RSA public key
-    oaep_padding = padding.OAEP(mgf=padding.MGF1(algorithm=SHA256()), algorithm=SHA256(), label=None)
-    cipherkey = public_key.encrypt(key, oaep_padding)
+    # opening the key
+    # string the key in a file
+    with open('encrypted.pdf', 'wb') as cipher:
+        cipher.write(ciphertext)
+    
 
+def aes_gcm_authenticated_decryption(cipherkey, iv, auth_tag, associated_data, ciphertext):
+    aes_gcm_decryptor = Cipher(AES(key), GCM(iv, auth_tag)).decryptor()
+    aes_gcm_decryptor.authenticate_additional_data(associated_data)
+    recovered_plaintext = aes_gcm_decryptor.update(ciphertext) + aes_gcm_decryptor.finalize()
+    # opening the file in write mode and
+    # writing the decrypted data
+    with open('recovered.pdf', 'wb') as dec_file:
+        dec_file.write(recovered_plaintext)
+
+if __name__ == "__main__":
+    
+    # Declare the key and IV
+    # 256-bit symmetric key
+    key = os.urandom(256 // 8)
+    # For AES GCM, NIST recommends 96 bit IVs
+    iv = os.urandom(96 // 8)
+    # Message to be kept confidential
+    # Associated data 
+    associated_data = b'Context of using AES GCM'
+    # Opening the original file to encrypt
+    with open('test.pdf', 'rb') as file:
+        plaintext = file.read()
+    # Store the key
+    with open('secret.key', 'wb') as key_file:
+        key_file.write(key) 
+
+    # Encrypt and authenticate the plaintext
+    aes_gcm_authenticated_encryption(key, iv, associated_data, plaintext)
+    
     # Decrypt and authenticate the ciphertext
-    recovered_plaintext = aes_gcm_authenticated_decryption(cipherkey, iv, auth_tag, associated_data, ciphertext, private_key)
-    assert (recovered_plaintext == plaintext)
-    print(recovered_plaintext)
+    with open('secret.key', 'rb') as secret:
+        key_for_decrypt = secret.read()
+    with open('encrypted.pdf', 'rb') as encrypted_file:
+        ciphertext = encrypted_file.read()
+    aes_gcm_authenticated_decryption(key_for_decrypt, iv, auth_tag, associated_data, ciphertext)
 
-    # Wrong key
-    wrong_key = os.urandom(256 // 8)
-    wrong_cipherkey = public_key.encrypt(wrong_key, oaep_padding)
-    try:
-        recovered_plaintext = aes_gcm_authenticated_decryption(wrong_cipherkey, iv, auth_tag, associated_data, ciphertext, private_key)
-    except cryptography.exceptions.InvalidTag:
-        pass
-    else:
-        # Should not happen
-        assert False
-
-    # Wrong iv
-    wrong_iv = os.urandom(96 // 8)
-    try:
-        recovered_plaintext = aes_gcm_authenticated_decryption(cipherkey, wrong_iv, auth_tag, associated_data, ciphertext, private_key)
-    except cryptography.exceptions.InvalidTag:
-        pass
-    else:
-        # Should not happen
-        assert False
-
-    # Wrong authentication tag
-    wrong_auth_tag = os.urandom(128 // 8)
-    try:
-        recovered_plaintext = aes_gcm_authenticated_decryption(cipherkey, iv, wrong_auth_tag, associated_data, ciphertext, private_key)
-    except cryptography.exceptions.InvalidTag:
-        pass
-    else:
-        # Should not happen
-        assert False
-
-    # Wrong associated data
-    wrong_associated_data = b'Wrong Context of using AES GCM'
-    try:
-        recovered_plaintext = aes_gcm_authenticated_decryption(cipherkey, iv, auth_tag, wrong_associated_data, ciphertext, private_key)
-    except cryptography.exceptions.InvalidTag:
-        pass
-    else:
-        # Should not happen
-        assert False
-
-    # Wrong ciphertext
-    wrong_ciphertext = ciphertext[:len(ciphertext)-1] + bytes([(ciphertext[-1] + 1) % 256])
-    try:
-        recovered_plaintext = aes_gcm_authenticated_decryption(cipherkey, iv, auth_tag, associated_data, wrong_ciphertext, private_key)
-    except cryptography.exceptions.InvalidTag:
-        pass
-    else:
-        # Should not happen
-        assert False
 
