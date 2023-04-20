7ace3fb258132f5e9274f6228d5d61db56a66b44
Hung Le Huy <63230297+Trumpiter-max@users.noreply.github.com>
2023-04-20 07:48:13 +0700

    update

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, send_from_directory
 from flask_pymongo import PyMongo
-from lib.aes_mode_gcm import encrypt, decrypt
+from bson.objectid import ObjectId
+import socket
+from utils.aes_mode_gcm import encrypt, decrypt
 import os
 
 app = Flask(__name__, static_folder='static')
+app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGO_INITDB_ROOT_USERNAME'] + ':' + os.environ['MONGO_INITDB_ROOT_PASSWORD'] + '@' + os.environ['MONGO_HOST'] + ':27017/' + os.environ['MONGO_INITDB_DATABASE']
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+mongodb = PyMongo(app)
 
-# Connect to MongoDB
-app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
-
-mongo = PyMongo(app)
-database = mongo.db
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
@@ -32,7 +32,36 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/todo')
+def todo():
+    _todos = mongodb.todo.find()
+    item = {}
+    data = []
+    for todo in _todos:
+        item = {
+            'id': str(todo['_id']),
+            'todo': todo['todo']
+        }
+        data.append(item)
+    return jsonify(
+        status=True,
+        data=data
+    )
+
+@app.route('/todo', methods=['POST'])
+def createTodo():
+    data = request.get_json(force=True)
+    item = {
+        'todo': data['todo']
+    }
+    mongodb.todo.insert_one(item)
+    return jsonify(
+        status=True,
+        message='To-do saved successfully!'
+    ), 201
+
 if __name__ == "__main__":
-    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
-    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG, ssl_context='secret')
+	env = os.environ.get('FLASK_ENV', 'production')
+	port = int(os.environ.get('PORT', 5000))
+	debug = False if env == 'production' else True
+	app.run(host='0.0.0.0', port=port, debug=debug)
